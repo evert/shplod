af630f50519ed2b744d867f0d41f00414326af76
Evert Pot <me@evertpot.com>
2015-06-16 22:19:36 -0400

    Removed purecss again until we really need it.

--- js/application.js
+++ js/application.js
@@ -2,8 +2,8 @@ Shplod.View.Application = Backbone.View.extend({
 
     initialize : function() {
 
-        this.$('#loader-panel').hide();
-        this.$('#login-panel').show();
+        this.$('#loader-panel').addClass('hidden');
+        this.$('#login-panel').removeClass('hidden');
 
     }
 
