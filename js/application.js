4d76df14513d1a5f241d6aa1ee7b93b679d961b1
Evert Pot <me@evertpot.com>
2015-06-16 22:11:46 -0400

    Application basics.

---
+++ js/application.js
@@ -0,0 +1,10 @@
+Shplod.View.Application = Backbone.View.extend({
+
+    initialize : function() {
+
+        this.$('#loader-panel').hide();
+        this.$('#login-panel').show();
+
+    }
+
+});
---
+++ js/init.js
@@ -0,0 +1,13 @@
+var Shplod = {
+    View : {}
+}
+
+
+$(document).ready(function() {
+
+    var application = new Shplod.View.Application({
+        el: $('body')
+    });
+
+});
+
